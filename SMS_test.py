2a698db57f5a9498ba9f1d42bf14f3b7e4e0da8f
napsquad <dtroutman2@live.esu.edu>
2018-01-29 14:55:01 -0500

    Update SMS_test.py
    oops

--- SMS_test.py
+++ SMS_test.py
@@ -1,11 +1,11 @@
 from twilio.rest import Client
 
-sid = "ACd0fe40ddfad1dd2313258d175732e860"
-auth_token= "7729b923d88c84b4c99921c2cfda0217"
+sid = ""
+auth_token= ""
 client = Client(sid, auth_token)  # twilio client
 
 message = client.messages.create(
-    "+19082794833",
+    "",
     body ="testing",
     from_= "+12015483972"
 )
