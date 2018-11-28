3259cb55fb9c14344890b2d18c48b60ef3b777cb
napsquad <dtroutman2@live.esu.edu>
2018-11-28 08:23:15 -0500

    remove database stuff
    no longer needed

--- meme.py
+++ meme.py
@@ -1,20 +1,17 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 from importmepls import ClothObj
-import sqlite3
 import time
 
 # attempts to make program appear human
 headers = {'User-Agent': 'Mozilla/5.0'}
 ClothObjs = []
 key = input("Brand to Search for: ")
-conn = sqlite3.connect("clothes.db")
 
-c= conn.cursor()
-target = ['https://www.reddit.com/r/frugalmalefashion/new/','https://www.reddit.com/r/deals/new/',
-          'https://www.reddit.com/r/SneakerDeals/new/', 'https://www.reddit.com/r/kicksmarket/new/',
-          'https://www.reddit.com/r/MaleFashionMarket/', 'https://www.reddit.com/r/ThriftyThread/',
-          'https://www.reddit.com/r/FashionRepsBST/']
+target = ['https://old.reddit.com/r/frugalmalefashion/new/','https://old.reddit.com/r/deals/new/',
+          'https://old.reddit.com/r/SneakerDeals/new/', 'https://old.reddit.com/r/kicksmarket/new/',
+          'https://old.reddit.com/r/MaleFashionMarket/', 'https://old.reddit.com/r/ThriftyThread/',
+          'https://old.reddit.com/r/FashionRepsBST/']
 
 for meme in target:
     try:
@@ -24,22 +21,6 @@ for meme in target:
         urls = []
         votes = []
         #used to hold Array of objects
-
-        def create_table():
-            c.execute('CREATE TABLE IF NOT EXISTS ClothingPlot(names TEXT, urls TEXT, votes REAL)')
-
-
-        def dynamic_data_entry(x):
-            y = names[x]
-            a = urls[x]
-            z = votes[x]
-
-            c.execute("INSERT INTO ClothingPlot(names, urls, votes) VALUES (?, ?, ?) ", (y, a, z))
-            conn.commit()
-
-
-        create_table()
-
 
         scrapey_Dad = urlopen(meme)
         page = scrapey_Dad.read()
@@ -58,27 +39,11 @@ for meme in target:
         for name in linkScout:
             votes.append(name.get_text())
 
-        for i in range(25):
-            dynamic_data_entry(i)
-
-
-        #counter = 0
-        #while True:
-         #  if (counter == 24):
-          #      break
-           # counter += 1
-            #print("Name: %s" % names[counter])
-            #print("URL: %s" % urls[counter])
-            #print("Votes: %s" % votes[counter])
-            #print()
-
         meme = ClothObj(names, votes, urls)
         ClothObjs.append(meme)
         meme.containsBrand(names, key, urls)  ## next get user input
 
     except:
         print("Blocked")
-        time.sleep(10)
+        time.sleep(2)
         continue
-c.close()
-conn.close()
